2e23c4c0456c56dc2bc08e9b7eebbeb641de62e7
Sasha Tsaplin <alexxxts@gmail.com>
2024-03-06 17:02:22 +0300

    Lesson 5. Final. Mini.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/alexxxts/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/alexxxts/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/7f43372dea67a3cbee68/maintainability)](https://codeclimate.com/github/alexxxts/frontend-project-44/maintainability)
+[![asciicast](https://asciinema.org/a/oTwgjBIbemO3c2W1CE6vMBrEJ.svg)](https://asciinema.org/a/oTwgjBIbemO3c2W1CE6vMBrEJ)
--- src/even-logic.js
+++ src/even-logic.js
@@ -4,15 +4,23 @@ export const checkAnswer = () => {
   let number = 0;
   let answer = '';
 
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
   for (let i = 0; i < 3; i += 1) {
-    number = Math.random();
+    number = Math.round(Math.random() * 100);
     console.log(`Question: ${number}`);
     answer = readlineSync.question('Your answer: ');
     if ((number % 2 === 0 && answer === 'yes') || (number % 2 !== 0 && answer === 'no')) {
       console.log('Correct!');
+    } else if (answer === 'yes') {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+      return `Let's try again, ${userName}!`;
     } else {
-      return false;
+      console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
+      return `Let's try again, ${userName}!`;
     }
-  };
-  return true;
+  }
+  return `Congratulations, ${userName}!`;
 };
