a9b42a49ac88a557f5f4d2edcf8ee983b5bb7a26
Sasha Tsaplin <alexxxts@gmail.com>
2024-03-07 21:31:05 +0300

    Lesson 6. Part 3. Mini.

--- src/games/even/even-start.js
+++ src/games/even/even-start.js
@@ -2,8 +2,6 @@ import { checkAnswer } from '../index.js';
 
 export const startEven = () => {
   const mainQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const randomExp = Math.round(Math.random() * 100);
-  const correctAnswer = (randomExp % 2 === 0) ? 'yes' : 'no';
 
-  return checkAnswer(mainQuestion, randomExp, correctAnswer);
+  return checkAnswer(mainQuestion);
 };
--- src/games/index.js
+++ src/games/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const checkAnswer = (mainQuestion, randomExp, correctAnswer) => {
+export const checkAnswer = (mainQuestion) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   let userAnswer = '';
