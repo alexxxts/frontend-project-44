a98dd9e7fea722afe727be86bcd2d06fccac1289
Sasha Tsaplin <alexxxts@gmail.com>
2024-03-07 10:27:36 +0300

    Lesson 6. Part 2. Mini.

--- src/games/even/even-start.js
+++ src/games/even/even-start.js
@@ -1,6 +1,6 @@
-import { checkAnswer } from './index.js';
+import { checkAnswer } from '../index.js';
 
-export const conditionsEven = () => {
+export const startEven = () => {
   const mainQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
   const randomExp = Math.round(Math.random() * 100);
   const correctAnswer = (randomExp % 2 === 0) ? 'yes' : 'no';
