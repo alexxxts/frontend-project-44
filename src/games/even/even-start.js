045cadf6969e8feff74d0a7338359e3d029dac08
Sasha Tsaplin <alexxxts@gmail.com>
2024-03-08 15:35:45 +0300

    Lesson 6. Part 4. Home.

--- src/games/even/even-start.js
+++ src/games/even/even-start.js
@@ -2,6 +2,17 @@ import { checkAnswer } from '../index.js';
 
 export const startEven = () => {
   const mainQuestion = 'Answer "yes" if the number is even, otherwise answer "no".';
+  const randomExp1 = Math.round(Math.random() * 100);
+  const correctAnswer1 = (randomExp1 % 2 === 0) ? 'yes' : 'no';
+  const randomExp2 = Math.round(Math.random() * 100);
+  const correctAnswer2 = (randomExp2 % 2 === 0) ? 'yes' : 'no';
+  const randomExp3 = Math.round(Math.random() * 100);
+  const correctAnswer3 = (randomExp3 % 2 === 0) ? 'yes' : 'no';
+  const evenMathArr = [
+    [randomExp1, correctAnswer1],
+    [randomExp2, correctAnswer2],
+    [randomExp3, correctAnswer3],
+  ];
 
-  return checkAnswer(mainQuestion);
+  return checkAnswer(mainQuestion, evenMathArr);
 };
