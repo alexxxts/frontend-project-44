ae79695a5a72d89646bba30580c2aaa79e06a872
Sasha Tsaplin <alexxxts@gmail.com>
2024-03-09 02:56:46 +0300

    Lesson 7. Final. Home.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/7f43372dea67a3cbee68/maintainability)](https://codeclimate.com/github/alexxxts/frontend-project-44/maintainability)
 [![asciicast](https://asciinema.org/a/oTwgjBIbemO3c2W1CE6vMBrEJ.svg)](https://asciinema.org/a/oTwgjBIbemO3c2W1CE6vMBrEJ)
 [![asciicast](https://asciinema.org/a/8y8bzJ01k5JJy3sq2foZJkz92.svg)](https://asciinema.org/a/8y8bzJ01k5JJy3sq2foZJkz92)
-
+[![asciicast](https://asciinema.org/a/SPu3S9RzcxYh0Dy2DoCai399N.svg)](https://asciinema.org/a/SPu3S9RzcxYh0Dy2DoCai399N)
---
+++ src/games/gcd/gcd-start.js
@@ -0,0 +1,27 @@
+import { checkAnswer } from '../index.js';
+
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
+
+export const startGcd = () => {
+  const mainQuestion = 'What is the result of the expression?';
+  const mathArr = [
+    [null, null],
+    [null, null],
+    [null, null],
+  ];
+  let a = 0;
+  let b = 0;
+
+  for (let i = 0; i < 3; i += 1) {
+    a = Math.round(Math.random() * 100);
+    b = Math.round(Math.random() * 100);
+    mathArr[i][0] = `${String(a)} ${String(b)}`;
+    mathArr[i][1] = String(gcd(a, b));
+  }
+  return console.log(checkAnswer(mainQuestion, mathArr));
+};
